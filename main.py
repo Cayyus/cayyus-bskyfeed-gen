61169dc3802be5f56e50a43b0d0edf8e14f5506f
Caius <117181766+Cayyus@users.noreply.github.com>
2025-04-25 10:38:45 +0600

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,19 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Message": "Hello, world"}
+@app.get("/xrpc/app.bsky.feed.describeFeedGenerator")
+async def describe_feed_generator():
+    return {
+        "did": "did:web:cayyus-bskyfeed-gen.onrender.com",
+        "feeds": [
+            {
+            "uri": "at://did:web:cayyus-bskyfeed-gen.onrender.com/app.bsky.feed.generator/my-feed",
+            "displayName": "Engineerverse by Cayyus"
+            }
+        ]
+    }
+
+
+@app.get("/.well-known/did.json")
+async def serve_did():
+    return FileResponse("did.json", media_type="application/json")
